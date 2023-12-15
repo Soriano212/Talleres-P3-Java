ef343e14be835da1ddfa853cf01ae1bac61bfc18
Alberto Soriano E <albertosoriano.e@gmail.com>
2023-12-15 08:29:16 -0500

    Agregado Canvas Java

--- Ejemplos/src/CanvasJava/CanvasPersonalizado.java
+++ Ejemplos/src/CanvasJava/CanvasPersonalizado.java
@@ -1,2 +1,20 @@
-package CanvasJava;public class CanvasPersonalizado {
+package CanvasJava;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CanvasPersonalizado extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // Realiza el dibujo personalizado aquí
+        g.setColor(Color.RED);
+        g.fillRect(50, 50, 100, 100);
+
+        g.setColor(Color.BLUE);
+        g.fillRect(0, 0, 50, 50);
+
+        g.fillOval(60, 60, 50, 100);
+    }
 }
+
--- Ejemplos/src/CanvasJava/Main.java
+++ Ejemplos/src/CanvasJava/Main.java
@@ -1,2 +1,19 @@
-package CanvasJava;public class Main {
+package CanvasJava;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Canvas en Java Swing");
+            frame.setSize(400, 400);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            CanvasPersonalizado canvas = new CanvasPersonalizado();
+            frame.add(canvas);
+
+            frame.setVisible(true);
+        });
+    }
 }
+
